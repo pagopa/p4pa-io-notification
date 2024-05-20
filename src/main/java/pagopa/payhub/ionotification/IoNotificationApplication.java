c9481312fa800996d704dc7ef05597620625668b
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-05-20 10:07:08 +0200

    P4ADEV-288-create-structure-new-microservice-io-notification

---
+++ src/main/java/pagopa/payhub/ionotification/IoNotificationApplication.java
@@ -0,0 +1,14 @@
+package pagopa.payhub.ionotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoNotificationApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pagopa/payhub/ionotification/P4paIoNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package pagopa.payhub.ionotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P4paIoNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
