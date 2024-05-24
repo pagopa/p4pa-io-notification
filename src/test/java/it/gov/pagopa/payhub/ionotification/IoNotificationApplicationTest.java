eece5f267866b6313462fdb0eadfd6b1756be192
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2024-05-24 10:02:48 +0200

    feat: P4ADEV-288-create-structure-new-microservice-io-notification (#2)
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288 add security-scan-pipelines.yml
    
    * P4ADEV-288P4ADEV-288-create-structure-new-microservice-io-notification
    
    * P4ADEV-288-create-structure-new-microservice-io-notification

---
+++ src/test/java/it/gov/pagopa/payhub/ionotification/IoNotificationApplicationTest.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.payhub.ionotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoNotificationApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
