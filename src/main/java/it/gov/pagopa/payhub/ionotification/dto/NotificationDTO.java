b6f17395c0a851527647e0164c4cd473cd6abccc
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-04 16:56:00 +0200

    P4ADEV-432-add-client-api-send-notification

---
+++ src/main/java/it/gov/pagopa/payhub/ionotification/dto/NotificationDTO.java
@@ -0,0 +1,28 @@
+package it.gov.pagopa.payhub.ionotification.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class NotificationDTO {
+
+    @Min(value = 3600)
+    @Max(value = 604800)
+    @JsonProperty("time_to_live")
+    private Long timeToLive;
+
+    @JsonProperty("fiscal_code")
+    @NotNull
+    @NotBlank
+    private String fiscalCode;
+
+    @NotNull private MessageContent content;
+}
---
+++ src/main/java/it/gov/pagopa/payhub/ionotification/dto/NotificationResource.java
@@ -0,0 +1,12 @@
+package it.gov.pagopa.payhub.ionotification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationResource {
+    private String id;
+}
