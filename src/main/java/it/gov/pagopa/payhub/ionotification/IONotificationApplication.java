0184535beadeebf5c57d30339a6dab50e22a5d47
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-17 10:19:36 +0200

    P4ADEV-449-api-create-service

--- src/main/java/it/gov/pagopa/payhub/ionotification/IONotificationApplication.java
+++ src/main/java/it/gov/pagopa/payhub/ionotification/IONotificationApplication.java
@@ -2,8 +2,10 @@ package it.gov.pagopa.payhub.ionotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "it.gov.pagopa.payhub.ionotification.repository")
 public class IONotificationApplication {
 
 
