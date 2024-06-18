54eeca6ab71a1acee90441aa37aff43d6dd99dcc
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-18 17:29:14 +0200

    P4ADEV-449-api-create-service fix logs

--- src/main/java/it/gov/pagopa/payhub/ionotification/service/ioservice/IODuplicateServiceHandlerService.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.payhub.ionotification.service.ioservice;
-
-import it.gov.pagopa.payhub.ionotification.model.IOService;
-import it.gov.pagopa.payhub.model.generated.ServiceRequestDTO;
-
-public interface IODuplicateServiceHandlerService {
-    String searchIOService(IOService service, ServiceRequestDTO serviceRequestDTO);
-}
