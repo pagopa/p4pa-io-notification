180f100f1670a740fbecbf8df5fa9f8c43b76d7c
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-18 18:24:18 +0200

    P4ADEV-522-handle-errors

---
+++ src/main/java/it/gov/pagopa/payhub/ionotification/exception/custom/IOWrongPayloadExceptiion.java
@@ -0,0 +1,7 @@
+package it.gov.pagopa.payhub.ionotification.exception.custom;
+
+public class IOWrongPayloadExceptiion extends RuntimeException{
+    public IOWrongPayloadExceptiion(String message){
+        super(message);
+    }
+}
