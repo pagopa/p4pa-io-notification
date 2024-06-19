409b29abbc476f7ad1e334ec172b213ba022dac2
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-19 18:09:53 +0200

    P4ADEV-457-api-send-notification fix

--- src/main/java/it/gov/pagopa/payhub/ionotification/event/producer/IONotificationProducer.java
+++ src/main/java/it/gov/pagopa/payhub/ionotification/event/producer/IONotificationProducer.java
@@ -3,13 +3,7 @@ package it.gov.pagopa.payhub.ionotification.event.producer;
 import it.gov.pagopa.payhub.model.generated.NotificationQueueDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-
-import java.util.function.Supplier;
 
 @Component
 public class IONotificationProducer {
@@ -23,13 +17,6 @@ public class IONotificationProducer {
         this.streamBridge = streamBridge;
     }
 
-    @Configuration
-    static class IONotificationProducerConfig {
-        @Bean
-        public Supplier<Flux<Message<Object>>> notificationQueue() {
-            return Flux::empty;
-        }
-    }
 
     public void sendNotification(NotificationQueueDTO notificationQueueDTO){
         streamBridge.send("notificationQueue-out-0", binder, notificationQueueDTO);
