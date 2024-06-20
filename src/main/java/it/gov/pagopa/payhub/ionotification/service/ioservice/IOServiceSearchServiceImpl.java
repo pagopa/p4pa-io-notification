2a2ed9d63753295f57bee0a5342d487b3d835687
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-06-20 16:54:12 +0200

    fix-get-all-services

--- src/main/java/it/gov/pagopa/payhub/ionotification/service/ioservice/IOServiceSearchServiceImpl.java
+++ src/main/java/it/gov/pagopa/payhub/ionotification/service/ioservice/IOServiceSearchServiceImpl.java
@@ -24,7 +24,16 @@ public class IOServiceSearchServiceImpl implements IOServiceSearchService {
     }
 
 
-    private ServicesListDTO getAllServicesWithPagination() {
+    @Override
+    public Optional<String> searchIOService(IOService service, ServiceRequestDTO serviceRequestDTO) {
+        log.info("Service request already exists, call IO to see if Service exists");
+        List<ServicePaginatedResponseDTO> services = retrieveAllServices();
+        Optional<ServicePaginatedResponseDTO> existingServiceOpt = findExistingService(service, services);
+
+        return existingServiceOpt.map(ServicePaginatedResponseDTO::getId);
+    }
+
+    private List<ServicePaginatedResponseDTO> retrieveAllServices() {
         List<ServicePaginatedResponseDTO> allServices = new ArrayList<>();
         boolean morePages = true;
         int offset = 0;
@@ -42,21 +51,11 @@ public class IOServiceSearchServiceImpl implements IOServiceSearchService {
             }
         }
 
-        return new ServicesListDTO(allServices, new PaginationDTO(offset, limit, allServices.size()));
+        return allServices;
     }
 
-    @Override
-    public Optional<String> searchIOService(IOService service, ServiceRequestDTO serviceRequestDTO) {
-        log.info("Service request already exists, call IO to see if Service exists");
-        ServicesListDTO servicesListDTO = getAllServicesWithPagination();
-        Optional<ServicePaginatedResponseDTO> existingServiceOpt = findExistingService(service, servicesListDTO);
-
-        return existingServiceOpt.map(ServicePaginatedResponseDTO::getId);
-    }
-
-
-    private Optional<ServicePaginatedResponseDTO> findExistingService(IOService service, ServicesListDTO servicesListDTO) {
-        return servicesListDTO.getServiceList().stream()
+    private Optional<ServicePaginatedResponseDTO> findExistingService(IOService service, List<ServicePaginatedResponseDTO> servicePaginatedResponseDTOS) {
+        return servicePaginatedResponseDTOS.stream()
                 .filter(existingService -> existingService.getServiceName().equals(service.getServiceName()) &&
                         existingService.getOrganization().getOrganizationName().equals(service.getOrganizationName()) &&
                         !existingService.getStatus().getValue().equals("deleted"))
