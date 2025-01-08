54f59bc5a22e1aaaa2bfeb6cdd4fcc33a2dc1716
antonio.torre <antonio.torre@pagopa.it>
2025-01-08 12:06:12 +0100

    P4ADEV-1754 configuring inner security

--- src/test/java/it/gov/pagopa/payhub/ionotification/controller/IONotificationControllerImplTest.java
+++ src/test/java/it/gov/pagopa/payhub/ionotification/controller/IONotificationControllerImplTest.java
@@ -7,6 +7,7 @@ import it.gov.pagopa.payhub.ionotification.dto.generated.ServiceDTO;
 import it.gov.pagopa.payhub.ionotification.dto.generated.ServiceRequestDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(IONotificationControllerImpl.class)
+@AutoConfigureMockMvc(addFilters = false)
 class IONotificationControllerImplTest {
 
     public static final Long TIPO_DOVUTO_ID = 456L;
