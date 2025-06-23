22fb9723a5e74464faac758c23dafa1e0608c39f
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-06-23 13:15:48 +0200

    fix: P4ADEV-3193 fix cve (#94)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
     java
-    id("org.springframework.boot") version "3.5.0"
+    id("org.springframework.boot") version "3.5.3"
     id("io.spring.dependency-management") version "1.1.7"
     jacoco
-    id("org.sonarqube") version "6.1.0.5360"
+    id("org.sonarqube") version "6.2.0.5505"
     id("com.github.ben-manes.versions") version "0.52.0"
     id("org.openapi.generator") version "7.13.0"
     id("com.gorylenko.gradle-git-properties") version "2.5.0"
@@ -39,7 +39,7 @@ dependencyManagement {
 val springDocOpenApiVersion = "2.8.9"
 val janinoVersion = "3.1.12"
 val openApiToolsVersion = "0.2.6"
-val wiremockVersion = "3.13.0"
+val wiremockVersion = "3.13.1"
 val hibernateValidatorVersion = "8.0.2.Final"
 val micrometerVersion = "1.5.1"
 val bouncycastleVersion = "1.81"
