98b7685fc324442187061eb9da9ffd58602a34b2
antonio.torre <antonio.torre@pagopa.it>
2025-12-19 16:36:49 +0100

    fix: Setting default TimeZone on main

--- src/main/java/it/gov/pagopa/payhub/ionotification/IONotificationApplication.java
+++ src/main/java/it/gov/pagopa/payhub/ionotification/IONotificationApplication.java
@@ -1,13 +1,17 @@
 package it.gov.pagopa.payhub.ionotification;
 
+import it.gov.pagopa.payhub.ionotification.utils.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
+
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class IONotificationApplication {
 
 	public static void main(String[] args) {
+        TimeZone.setDefault(Constants.DEFAULT_TIMEZONE);
 		SpringApplication.run(IONotificationApplication.class, args);
 	}
 
