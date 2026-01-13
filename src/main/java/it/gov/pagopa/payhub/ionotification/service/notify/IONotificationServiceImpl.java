6f716664a0ec6dd65a37479b2aa78bd8f865d16c
antocalo <antonio.calo@nttdata.com>
2026-01-13 15:19:17 +0100

    P4ADEV-4342-check-cf-validity-when-send-io-notification

--- src/main/java/it/gov/pagopa/payhub/ionotification/service/notify/IONotificationServiceImpl.java
+++ src/main/java/it/gov/pagopa/payhub/ionotification/service/notify/IONotificationServiceImpl.java
@@ -33,6 +33,7 @@ public class IONotificationServiceImpl implements IONotificationService {
     private final OrganizationService organizationService;
     private final Long timeToLive;
 
+    @SuppressWarnings("java:S5843")
     private static final String CF_VALIDITY_REGEX = "^(?:[A-Z][AEIOUX][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$";
     private static final Pattern CF_PATTERN = Pattern.compile(CF_VALIDITY_REGEX);
 
