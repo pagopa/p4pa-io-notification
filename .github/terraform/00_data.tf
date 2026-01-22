97d2d86bacebcbd9c68f7c693dc8074d3ce735cf
antonio.torre <antonio.torre@pagopa.it>
2026-01-22 17:08:48 +0100

    chore: Added AZURE_DEVOPS_TOKEN on GitHub Action secrets through terraform

--- .github/terraform/00_data.tf
+++ .github/terraform/00_data.tf
@@ -4,16 +4,16 @@ data "github_organization_teams" "all" {
   summary_only    = true
 }
 
-# Secret
-data "azurerm_key_vault" "key_vault" {
-  name                = "${var.prefix}-${var.env_short}-${var.location_short}-core-kv"
-  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-core-sec-rg"
-}
-
 # KV Core
 data "azurerm_key_vault" "key_vault_core" {
   name                = "${var.prefix}-${var.env_short}-${var.location_short}-core-kv"
   resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-core-sec-rg"
+}
+
+# Kv Domain
+data "azurerm_key_vault" "key_vault_domain" {
+  name                = "${var.prefix}-${var.env_short}-${var.domain}-kv"
+  resource_group_name = "${var.prefix}-${var.env_short}-${var.location_short}-${var.domain}-sec-rg"
 }
 
 # Key Vault - Sonar Token
